93b97fdd9417039acb3c57432372ba26a62a08e0
Kevin Goodenough <kevingoodenough@Kevins-MBP.lan>
2020-09-24 11:04:20 -0500

    Updates

---
+++ precache-manifest.a192e95d125c06ab360c0b48a558e15e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96a176af9de4ba4a3b655d14beec4ff",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b12e6cc6b5954841d067",
+    "url": "/monsters-rolodex/static/css/main.afff1e98.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b12e6cc6b5954841d067",
+    "url": "/monsters-rolodex/static/js/main.34f1d335.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
